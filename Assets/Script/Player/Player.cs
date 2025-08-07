83f0246dd9f059342064faaf489f369dd1d374db
SJL2002 <sungjunlim13@gmail.com>
2025-08-07 12:24:36 +0900

    Initial commit

---
+++ Assets/Script/Player/Player.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public PlayerController controller;
+    public PlayerCondition condition;
+
+    public ItemData itemData;
+    public Action addItem;
+
+
+
+    private void Awake()
+    {
+        CharacterManager.Instance.Player = this;
+        controller = GetComponent<PlayerController>();
+        condition = GetComponent<PlayerCondition>();
+
+    }
+}
---
+++ Assets/Script/Player/PlayerController.cs
@@ -0,0 +1,142 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed;
+    private Vector2 curMovementInput;
+    public float jumpPower;
+    public LayerMask groundLayerMask;
+
+    [Header("Look")]
+    public Transform cameraContainer;
+    public float minXLook;
+    public float maxXLook;
+    private float camCurXRot;
+    public float lookSensitivity;
+
+    private Vector2 mouseDelta;
+
+    [HideInInspector]
+    public bool canLook = true;
+
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; //Wether to lock the mouse or not (Make it invisible)
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void LateUpdate()
+    {
+        if (canLook)
+        {
+            CameraLook();
+        }
+    }
+
+    public void OnLookInput(InputAction.CallbackContext context)
+    {
+        mouseDelta = context.ReadValue<Vector2>();
+    }
+
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            curMovementInput = context.ReadValue<Vector2>();
+        }
+        else if (context.phase == InputActionPhase.Canceled)
+        {
+            curMovementInput = Vector2.zero;
+        }
+    }
+
+    public void OnJumpInput(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started && IsGrounded())
+        {
+            rb.AddForce(Vector2.up * jumpPower, ForceMode.Impulse);
+        }
+    }
+
+    private void Move()
+    {
+        Vector3 dir = transform.forward * curMovementInput.y + transform.right * curMovementInput.x;
+        dir *= moveSpeed;
+        dir.y = rb.velocity.y;
+
+        rb.velocity = dir;
+    }
+
+    void CameraLook()
+    {
+        camCurXRot += mouseDelta.y * lookSensitivity; // Y 값에 민감도를 곱하며, 
+        camCurXRot = Mathf.Clamp(camCurXRot, minXLook, maxXLook); // Clamp을 통해서, 회전을 제한한다
+        cameraContainer.localEulerAngles = new Vector3(-camCurXRot, 0, 0); // 적용한 것을 camcotainer의 x값에 한다 (위 아래)
+
+        transform.eulerAngles += new Vector3(0, mouseDelta.x * lookSensitivity, 0); // 현재 x 각도를 유지를하면서, Y (양 옆)을 추가하면서, 상하좌우의 Look을 구현한다.
+    }
+
+    bool IsGrounded()
+    {
+        Ray[] rays = new Ray[4]
+        {
+            // 플레이어의 앞, 뒤, 옆을 안 보이는 Ray를 통해, 땅 위에 있는지를 판단.
+            new Ray(transform.position + (transform.forward * 0.2f) + (transform.up * 0.01f), Vector3.down),
+            new Ray(transform.position + (-transform.forward * 0.2f) + (transform.up * 0.01f), Vector3.down),
+            new Ray(transform.position + (transform.right * 0.2f) + (transform.up * 0.01f), Vector3.down),
+            new Ray(transform.position + (-transform.right * 0.2f) + (transform.up * 0.01f), Vector3.down)
+        };
+
+        for (int i = 0; i < rays.Length; i++)
+        {
+            if (Physics.Raycast(rays[i], 0.1f, groundLayerMask))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void ToggleCursor(bool toggle)
+    {
+        Cursor.lockState = toggle ? CursorLockMode.None : CursorLockMode.Locked;
+        canLook = !toggle;
+    }
+
+    //Visual Learning.
+    // void DrawGroundRays()
+    // {
+    //     Vector3 origin = transform.position + (transform.up * 0.01f);
+    //     float offset = 0.2f;
+    //     float rayLength = 0.3f;
+
+    //     // Define four ray origins
+    //     Vector3[] rayOrigins = new Vector3[]
+    //     {
+    //         origin + (transform.forward * offset),   // Front
+    //         origin + (-transform.forward * offset),  // Back
+    //         origin + (transform.right * offset),     // Right
+    //         origin + (-transform.right * offset)     // Left
+    //     };
+
+    //     foreach (Vector3 start in rayOrigins)
+    //     {
+    //         Debug.DrawRay(start, Vector3.down * rayLength, Color.red);
+    //     }
+    // }
+}
