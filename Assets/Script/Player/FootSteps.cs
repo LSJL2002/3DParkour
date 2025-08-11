fe59ddac948a4991ccda71201b2bc0a5fa7f77ae
SJL2002 <sungjunlim13@gmail.com>
2025-08-11 12:32:05 +0900

    [ Init ] BGM and SFX

--- Assets/Script/Player/FootSteps.cs
+++ Assets/Script/Player/FootSteps.cs
@@ -1,18 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FootSteps : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioClip[] footstepClips;
+    private AudioSource audioSource;
+    private Rigidbody _rigidbody;
+    public float footstepThreshold;
+    public float footstepRate;
+    private float footStepTime;
+
+    private void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(Mathf.Abs(_rigidbody.velocity.y) < 0.1f) //If grounded 
+        {
+            if(_rigidbody.velocity.magnitude > footstepThreshold) //and is moving then play the audio clip
+            {
+                if(Time.time - footStepTime > footstepRate) //Play the audio every footstepRate.
+                {
+                    footStepTime = Time.time;
+                    audioSource.PlayOneShot(footstepClips[Random.Range(0, footstepClips.Length)]);
+                }
+            }
+        }
     }
 }
