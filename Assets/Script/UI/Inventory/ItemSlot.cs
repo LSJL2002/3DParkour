f06a755604092f8dbf5251da48f8c8594054eaf9
SJL2002 <sungjunlim13@gmail.com>
2025-08-14 11:20:11 +0900

    [ Modify ] Slot Drag and Drop

--- Assets/Script/UI/Inventory/ItemSlot.cs
+++ Assets/Script/UI/Inventory/ItemSlot.cs
@@ -1,11 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
 using UnityEngine.UI;
-using System.Linq;
 
-public class ItemSlot : MonoBehaviour
+public class ItemSlot : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IDropHandler
 {
     public ItemData item;
     public UIInventory inventory;
@@ -17,9 +15,18 @@ public class ItemSlot : MonoBehaviour
     public int index;
     public bool equipped;
     public int quantity;
+
+    private Image draggedIcon;
+    private Canvas parentCanvas;
+
     void Awake()
     {
         outline = GetComponent<Outline>();
+    }
+
+    void Start()
+    {
+        parentCanvas = GetComponentInParent<Canvas>();
     }
 
     private void OnEnable()
@@ -32,11 +39,8 @@ public class ItemSlot : MonoBehaviour
         icon.gameObject.SetActive(true);
         icon.sprite = item.icon;
         quantityText.text = quantity > 1 ? quantity.ToString() : string.Empty;
-
         if (outline != null)
-        {
             outline.enabled = equipped;
-        }
     }
 
     public void Clear()
@@ -49,5 +53,65 @@ public class ItemSlot : MonoBehaviour
     public void onClickButton()
     {
         inventory.SelectItem(index);
+    }
+
+    // === Drag & Drop ===
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (item == null) return;
+
+        // Create temporary dragged icon
+        draggedIcon = new GameObject("DraggedIcon", typeof(Image)).GetComponent<Image>();
+        draggedIcon.transform.SetParent(parentCanvas.transform, false);
+        draggedIcon.sprite = icon.sprite;
+        draggedIcon.rectTransform.sizeDelta = icon.rectTransform.sizeDelta;
+        draggedIcon.raycastTarget = false;
+
+        icon.enabled = false; // hide original icon
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (draggedIcon != null)
+        {
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                parentCanvas.transform as RectTransform,
+                eventData.position,
+                eventData.pressEventCamera,
+                out Vector2 localPoint
+            );
+            draggedIcon.rectTransform.localPosition = localPoint;
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (draggedIcon != null)
+        {
+            Destroy(draggedIcon.gameObject);
+            draggedIcon = null;
+        }
+        icon.enabled = true; // show original icon again
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        var fromSlot = eventData.pointerDrag?.GetComponent<ItemSlot>();
+        if (fromSlot != null && fromSlot != this)
+        {
+            var tempItem = item;
+            var tempQuantity = quantity;
+            var tempEquipped = equipped;
+
+            item = fromSlot.item;
+            quantity = fromSlot.quantity;
+            equipped = fromSlot.equipped;
+
+            fromSlot.item = tempItem;
+            fromSlot.quantity = tempQuantity;
+            fromSlot.equipped = tempEquipped;
+
+            inventory.UpdateUI();
+        }
     }
 }
